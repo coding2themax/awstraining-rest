4c1922c338d465ff4a8f0c0673e17cc15acdd11a
max quinones <maximilian.quinones@gmail.com>
2023-04-08 07:07:42 -0700

    spring

---
+++ src/main/java/coding2themax/aws/training/springsample/SpringSampleApplication.java
@@ -0,0 +1,13 @@
+package coding2themax.aws.training.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coding2themax/aws/training/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package coding2themax.aws.training.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
